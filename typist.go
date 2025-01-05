1b2ffcb6ad7d8aa3f3c8abf9c115edcb2f00738c
George Honeywood <george.honeywood.2019@rhul.ac.uk>
2025-01-05 23:11:52 +0000

    feat: figured out the epson control codes!

--- typist.go
+++ typist.go
@@ -1,5 +1,9 @@
 package main
 
+// very useful control code reference
+// my printer is in epson emulation mode, so most of the ESC/P commands should work
+// https://www.whitefiles.org/dta/pgs/cc03c_prntr_cds.pdf
+
 import (
 	"fmt"
 	"os"
@@ -8,65 +12,92 @@ import (
 	"golang.org/x/term"
 )
 
+func reader(char chan byte) {
+	buf := make([]byte, 1)
+	for {
+		i, err := os.Stdin.Read(buf)
+		fmt.Printf("read %d bytes\r\n", i)
+		if err != nil {
+			panic(err)
+		}
+
+		char <- buf[0]
+	}
+}
+
 func main() {
-	// open input file
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
 		panic(err)
 	}
 	defer term.Restore(int(os.Stdin.Fd()), oldState)
 
-	// open output file
-	fo, err := os.Create("/dev/usb/lp0")
+	lp, err := os.Create("/dev/usb/lp0")
 	if err != nil {
 		panic(err)
 	}
-	// close fo on exit and check for its returned error
 	defer func() {
-		if err := fo.Close(); err != nil {
+		if err := lp.Close(); err != nil {
 			panic(err)
 		}
 	}()
 
-	lastWrite := time.Now()
+	scrolledUp := false
 
-	buf := make([]byte, 1)
+	pipe := make(chan byte)
+	go reader(pipe)
+
+	// ESC/P typewriter mode not emulated :(
+	// fo.Write([]byte{27, 105, 1})
+
+	// the idea here is that we can't read characters immediately behind the
+	// printer head, so we need to scroll down to read them. we can't do this
+	// immediately, so we wait for a timeout
+readLoop:
 	for {
-		time.Since(lastWrite)
-		// read a chunk
-		i, err := os.Stdin.Read(buf)
-		fmt.Printf("read %d bytes\n", i)
-		if err != nil {
-			panic(err)
+		select {
+		case char := <-pipe:
+			if char == 3 {
+				fmt.Printf("got ctrl+c\r\n")
+				break readLoop
+			}
+
+			if char == 13 {
+				fmt.Printf("got enter\r\n")
+				// line feed
+				lp.Write([]byte{10})
+				continue
+			}
+
+			fmt.Printf("writing %d\r\n", char)
+
+			if scrolledUp {
+				// if scrolled up, scroll back down
+				fmt.Print("scrolling back down\r\n")
+
+				// 27 106 nn: Perform nn/216 inch reverse line feed
+				lp.Write([]byte{27, 106, 216})
+				lp.Write([]byte{27, 106, 216})
+				scrolledUp = false
+			}
+
+			// write a character
+			_, err = lp.Write([]byte{char})
+			if err != nil {
+				panic(err)
+			}
+
+		case <-time.After(time.Second * 2):
+			if scrolledUp {
+				break
+			}
+			fmt.Print("timed out, scrolling up\r\n")
+
+			// 27 74 nn: Perform nn/216 inch line feed
+			lp.Write([]byte{27, 74, 216})
+			lp.Write([]byte{27, 74, 216})
+			scrolledUp = true
 		}
-
-		// check for eof
-		if i == 0 {
-			fmt.Printf("got eof\n")
-			break
-		}
-
-		if buf[0] == 3 {
-			fmt.Printf("got ctrl-c\n")
-			break
-		}
-
-		if buf[0] == 13 {
-			fmt.Printf("got enter\n")
-			fo.Write([]byte{10})
-			continue
-		}
-
-		fmt.Printf("writing %d\n", buf[0])
-
-		// write a chunk
-		_, err = fo.Write(buf)
-		if err != nil {
-			panic(err)
-		}
-
-		// fo.Write([]byte{27, 74, 216})
-		fo.Write([]byte{27, 93})
 
 	}
 }
